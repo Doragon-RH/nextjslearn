5ea5bc9f77fddc8d1b7d2a33fa515bd387aabc0e
Doragon-RH <r216af@icloud.com>
2023-12-29 10:51:41 +0900

    ページの追加

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
 こちらでは、Reactを学習したのちにNext.jsによるアプリ作成方法について学習していきます。
 学習記録は以下のzennの記事にまとめます。(順次公開予定)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
@@ -11,13 +13,12 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">This page!</Link>
         </h1>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
-
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
