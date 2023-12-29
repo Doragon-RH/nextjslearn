9f217b88c776125124195112b3ef56ea702257b7
Doragon-RH <r216af@icloud.com>
2023-12-29 14:33:31 +0900

    loadについてのコード記述

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
         <>
             <Head>
-                <title>First Post</title>
+                <title>First Post</title> {/*  タブの表示変更 */}
             </Head>
+            <Script
+            src = "https://connect.facebook.net/en_US/sdk.js"
+            strategy='lazyOnload' /*  lazyOnload でスクリプトの読み込みを遅らせる */
+            onLoad={() =>
+                console.log('script Loaded correctly, window.FB has been populated')
+            } /* しっかりスクリプトの読み込みが完了したのちにjsコードを実行するために使用 */
+            />
             <h1>First Post</h1>
             <h2>
                 <Link href="/">
